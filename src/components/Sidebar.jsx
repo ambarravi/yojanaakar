fcaaefd16228a4f9b241a9c7689903c77899656e
Ravi Ambar <ravi.ambar@gmail.com>
2025-02-18 12:01:49 +0530

    Udpated Signout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,10 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import { fetchAuthSession, signOut } from "@aws-amplify/auth";
 
 import "../styles/Sidebar.css";
 
-const Sidebar = ({ signOut, className }) => {
+const Sidebar = ({ className }) => {
   const { user, loading } = useContext(AuthContext); // Access user from AuthContext
   const navigate = useNavigate();
 
@@ -18,9 +19,19 @@ const Sidebar = ({ signOut, className }) => {
     navigate("/host-event", { state: { fromSidebar: true } });
   };
 
-  const handleLogout = () => {
-    signOut();
-    navigate("/");
+  const handleLogout = async () => {
+    // const signOutUrl = `https://eventmgmt.auth.eu-west-1.amazoncognito.com/logout?client_id=eventmgmt&logout_uri=${encodeURIComponent(
+    //   window.location.origin
+    // )}`;
+    await signOut({ global: true });
+
+    // await signOut();
+
+    sessionStorage.clear(); // Clear session data
+    localStorage.clear(); // Clear local storage tokens
+    console.log(await fetchAuthSession()); // Should return an empty session
+    // window.location.href = signOutUrl; // Redirect to Cognito's logout URL
+    window.location.href = "/";
   };
 
   return (
@@ -57,7 +68,7 @@ const Sidebar = ({ signOut, className }) => {
       </div>
       <ul className="nav flex-column">
         <li className="nav-item">
-          <Link to="/manage-events" className="nav-link text-dark">
+          <Link to="/organizer-landing" className="nav-link text-dark">
             <i className="bi bi-house-door"></i> Home
           </Link>
         </li>
