bb44ea2e30fb3517ec65fd5dc0f41419cd638c4f
Ravi Ambar <ravi.ambar@gmail.com>
2025-08-12 18:46:45 +0530

    Udpated scanner

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import "../styles/ManageEvent.css";
 import { Html5Qrcode } from "html5-qrcode";
